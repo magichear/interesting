3a89852334e9a318eb1decd6f94c40048e5c711a
mo Huanxin <133361752+magichear@users.noreply.github.com>
2023-09-28 14:03:31 +0800

    Update index.js

--- firework/js/index.js
+++ firework/js/index.js
@@ -22,7 +22,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||莫|大|帅|哥|祝|周|星|宇|节|日|快|乐|#rectangle|");
+      S.UI.simulate("|#countdown 3||莫|大|帅|哥|祝|你|节|日|快|乐|#rectangle|");
     }
 
     S.Drawing.loop(function () {
